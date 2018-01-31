41660ec20e72266ba332b1f39a256ad7011a90c0
Victor Jin <victor.jin@oracle.com>
2018-01-30 20:38:14 -0800

    create maven project and add sample code

---
+++ src/main/java/codinghigh/algorithms/leetcode/GlobalAndLocalInversions.java
@@ -0,0 +1,10 @@
+package codinghigh.algorithms.leetcode;
+
+/**
+ * Created by xinweijin on 1/28/18.
+ * leetcode #775
+ * https://leetcode.com/problems/global-and-local-inversions/description/
+ */
+public class GlobalAndLocalInversions {
+    //TODO 
+}
---
+++ src/test/java/codinghigh/algorithms/leetcode_unit_test/AddTwoNumbersTest.java
@@ -0,0 +1,22 @@
+package codinghigh.algorithms.leetcode_unit_test;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by xinweijin on 1/30/18.
+ */
+public class AddTwoNumbersTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+}
