acc915f66148582170c7b34b3605cb67e2262c68
Victor Jin <victor.jin@oracle.com>
2018-02-04 14:17:12 -0800

    resolve TwoSum

--- src/main/java/codinghigh/algorithms/leetcode/TwoSum.java
+++ src/main/java/codinghigh/algorithms/leetcode/TwoSum.java
@@ -1,4 +1,12 @@
+/*
+ * Copyright (c) 2018 to Xinwei Jin.
+ * All Rights Reserved.
+ */
+
 package codinghigh.algorithms.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by xinweijin on 1/28/18.
@@ -6,4 +14,64 @@ package codinghigh.algorithms.leetcode;
  * https://leetcode.com/problems/two-sum/description/
  */
 public class TwoSum {
+    /**
+     * If the array is sorted in increasing order
+     * two pointer (p1 -> the first number, p2 -> the second number)
+     * if (nums[p1] + nums[p2] > target) move p2 to left -> p2--;
+     * if (nums[p1] + nums[p2] < target) move p1 to right -> p1++;
+     * if (nums[p1] + nums[p2] == target) find the result;
+     *
+     * @param nums input array
+     * @param target target sum
+     * @return the index array of the first and second number
+     */
+    public int[] twoSum1(int[] nums, int target) {
+        int[] result = new int[2];
+        if (nums == null || nums.length == 0) {
+            return result;
+        }
+        int length = nums.length;
+        int left = 0;
+        int right = length - 1;
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+            if (sum == target) {
+                result[0] = left;
+                result[1] = right;
+                return result;
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * map<nums[i], i>
+     * interate the nums array
+     *  if the map contains the key target - nums[i], then return the result;
+     *  push the pair<nums[i], i> to map
+     *
+     * @param nums input array
+     * @param target target sum
+     * @return the index array of the first and second number
+     */
+    public int[] twoSum2(int[] nums, int target) {
+        int[] result = new int[2];
+        if (nums == null || nums.length == 0) {
+            return result;
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                result[0] = map.get(target - nums[i]);
+                result[1] = i;
+                return result;
+            }
+            map.put(nums[i], i);
+        }
+        return result;
+    }
 }
--- src/test/java/codinghigh/algorithms/leetcode_unit_test/TwoSumTest.java
+++ src/test/java/codinghigh/algorithms/leetcode_unit_test/TwoSumTest.java
@@ -1,14 +1,28 @@
+/*
+ * Copyright (c) 2018 to Xinwei Jin.
+ * All Rights Reserved.
+ */
+
 package codinghigh.algorithms.leetcode_unit_test;
 
+import codinghigh.algorithms.leetcode.TwoSum;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 /**
  * Created by xinweijin on 1/30/18.
+ * unit test for TwoSum
  */
 public class TwoSumTest {
+    private final int[] DEFAULT_TEST_CASE_NUMS_1 = {1, 6, 8, 14, 19, 24};
+    private final int DEFAULT_TEST_CASE_TARGET_1 = 22;
+    private final int[] DEFAULT_TEST_CASE_NUMS_2 = {6, 1, 8, 24, 19, 14};
+    private final int DEFAULT_TEST_CASE_TARGET_2 = 22;
+    private TwoSum twoSum = new TwoSum();
+
     @Before
     public void setUp() throws Exception {
 
@@ -19,4 +33,15 @@ public class TwoSumTest {
 
     }
 
+    @Test
+    public void twoSum1_increaseInputArray_shouldWork() {
+        int[] result = {2, 3};
+        assertArrayEquals(result, twoSum.twoSum1(DEFAULT_TEST_CASE_NUMS_1, DEFAULT_TEST_CASE_TARGET_1));
+    }
+
+    @Test
+    public void twoSum2_uniqueElementArray_shouldWork() {
+        int[] result = {2, 5};
+        assertArrayEquals(result, twoSum.twoSum2(DEFAULT_TEST_CASE_NUMS_2, DEFAULT_TEST_CASE_TARGET_2));
+    }
 }
